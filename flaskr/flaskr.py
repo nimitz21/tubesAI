6f08b3b5b53ecddf1a24a051e89f3b064ae7cc7c
Rionaldi Chandraseta <rionaldi.chandraseta@gmail.com>
2017-11-28 16:45:21 +0700

    Added models, label encoders, minor fix

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -22,6 +22,9 @@ def showPage():
 
 @app.route('/submit', methods=['POST'])
 def submit():
+	for form in request.form:
+		print(form)
+
 	model_dir_path = os.path.dirname(os.path.realpath('model_id3.pkl'))
 	model = tree.DecisionTreeClassifier()
 	model = joblib.load(model_dir_path + '\\flaskr\\model_id3.pkl')
@@ -32,12 +35,17 @@ def submit():
 	discrete_values = []
 
 	for column in discrete_columns:
-		label_encoder.classes_ = numpy.load(column + '.npy')
-		discrete_values.append(label_encoder.transform([request.form['age']])[0])
+		label_encoder.classes_ = np.load(model_dir_path + '\\flaskr\\' + column + '.npy')
+		discrete_values.append(label_encoder.transform([request.form[column]]).astype(int)[0])
+		print(label_encoder.transform([request.form[column]])[0])
 
-	dataFrame = pd.DataFrame([request.form['age'], discrete_values[0], discrete_values[1], discrete_values[2], discrete_values[3], discrete_values[4], discrete_values[5], request.form['capital-gain'], request.form['capital-loss'], request.form['hours-per-week'], request.form['income']], columns=['age', 'workclass', 'education', 'marital-status', 'occupation', 'relationship', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'income'])
-	
+	print('TEST')
+	print(discrete_values[0])
+
+	dataFrame = pd.DataFrame([request.form['age'], discrete_values[0], discrete_values[1], discrete_values[2], discrete_values[3], discrete_values[4], discrete_values[5], request.form['capital-gain'], request.form['capital-loss'], request.form['hours-per-week']], columns=['age', 'workclass', 'education', 'marital-status', 'occupation', 'relationship', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week'])
+	print(dataFrame)
+
 	result = model.predict(dataFrame)
-	return redirect(url_for('showPage'), result=result)
+	return redirect(url_for('index'), result=result)
 
 
